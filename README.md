28d793b053baa491a0d39c6fc3f9f14f4d28faba
Toms Mikoss <toms.mikoss@gmail.com>
2016-05-16 14:29:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Descripton
+Can't use `belongs_to` when the relation ID is saved by a store accessor
+
+# Usage
+Run `ruby test.rb`
---
+++ test.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'bundler/setup'
+
+Bundler.require(:default)
+
+ActiveRecord::Base.establish_connection(adapter: 'postgresql', database: 'template1')
+
+class User < ActiveRecord::Base
+end
+
+class Comment < ActiveRecord::Base
+  store_accessor :data, :user_id
+
+  belongs_to :user
+end
+
+ActiveRecord::Base.transaction do
+  ActiveRecord::Base.connection.create_table :users
+
+  ActiveRecord::Base.connection.create_table :comments do |t|
+    t.json :data
+  end
+
+  user = User.create!
+
+  comment = Comment.new
+  comment.user_id = user.id
+  comment.save!
+
+  p "Attribute gets set: #{comment.user_id == user.id}"
+  p "Can use belongs_to relation: #{comment.user == user}"
+  p "Can use belongs_to relation after reload: #{comment.reload.user == user}"
+
+  raise ActiveRecord::Rollback
+end
